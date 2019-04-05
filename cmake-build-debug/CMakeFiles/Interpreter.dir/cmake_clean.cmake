93bf34c638fcd31a99b8178b76976d1acc9a09f7
Anton Vovk <archwolf@protonmail.com>
2019-04-05 23:24:48 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Interpreter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Interpreter.dir/main.cpp.o"
+  "Interpreter.pdb"
+  "Interpreter"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Interpreter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
