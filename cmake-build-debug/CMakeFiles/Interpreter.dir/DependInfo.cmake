c2ace30bf8f4732caa9a8f96553dc582f1b38397
Anton Vovk <archwolf@protonmail.com>
2019-04-06 23:41:09 +0300

    CI/CD is hard

---
+++ cmake-build-debug/CMakeFiles/Interpreter.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wolf/Documents/Interpreter/Expressions/Assign.cpp" "/home/wolf/Documents/Interpreter/cmake-build-debug/CMakeFiles/Interpreter.dir/Expressions/Assign.cpp.o"
+  "/home/wolf/Documents/Interpreter/Expressions/Binary.cpp" "/home/wolf/Documents/Interpreter/cmake-build-debug/CMakeFiles/Interpreter.dir/Expressions/Binary.cpp.o"
+  "/home/wolf/Documents/Interpreter/Expressions/Call.cpp" "/home/wolf/Documents/Interpreter/cmake-build-debug/CMakeFiles/Interpreter.dir/Expressions/Call.cpp.o"
+  "/home/wolf/Documents/Interpreter/Expressions/Get.cpp" "/home/wolf/Documents/Interpreter/cmake-build-debug/CMakeFiles/Interpreter.dir/Expressions/Get.cpp.o"
+  "/home/wolf/Documents/Interpreter/Expressions/Grouping.cpp" "/home/wolf/Documents/Interpreter/cmake-build-debug/CMakeFiles/Interpreter.dir/Expressions/Grouping.cpp.o"
+  "/home/wolf/Documents/Interpreter/Expressions/Literal.cpp" "/home/wolf/Documents/Interpreter/cmake-build-debug/CMakeFiles/Interpreter.dir/Expressions/Literal.cpp.o"
+  "/home/wolf/Documents/Interpreter/Expressions/Logical.cpp" "/home/wolf/Documents/Interpreter/cmake-build-debug/CMakeFiles/Interpreter.dir/Expressions/Logical.cpp.o"
+  "/home/wolf/Documents/Interpreter/Expressions/Set.cpp" "/home/wolf/Documents/Interpreter/cmake-build-debug/CMakeFiles/Interpreter.dir/Expressions/Set.cpp.o"
+  "/home/wolf/Documents/Interpreter/Expressions/Super.cpp" "/home/wolf/Documents/Interpreter/cmake-build-debug/CMakeFiles/Interpreter.dir/Expressions/Super.cpp.o"
+  "/home/wolf/Documents/Interpreter/Expressions/This.cpp" "/home/wolf/Documents/Interpreter/cmake-build-debug/CMakeFiles/Interpreter.dir/Expressions/This.cpp.o"
+  "/home/wolf/Documents/Interpreter/Expressions/Unary.cpp" "/home/wolf/Documents/Interpreter/cmake-build-debug/CMakeFiles/Interpreter.dir/Expressions/Unary.cpp.o"
+  "/home/wolf/Documents/Interpreter/Expressions/Variable.cpp" "/home/wolf/Documents/Interpreter/cmake-build-debug/CMakeFiles/Interpreter.dir/Expressions/Variable.cpp.o"
+  "/home/wolf/Documents/Interpreter/Main.cpp" "/home/wolf/Documents/Interpreter/cmake-build-debug/CMakeFiles/Interpreter.dir/Main.cpp.o"
+  "/home/wolf/Documents/Interpreter/Printer.cpp" "/home/wolf/Documents/Interpreter/cmake-build-debug/CMakeFiles/Interpreter.dir/Printer.cpp.o"
+  "/home/wolf/Documents/Interpreter/Scanner.cpp" "/home/wolf/Documents/Interpreter/cmake-build-debug/CMakeFiles/Interpreter.dir/Scanner.cpp.o"
+  "/home/wolf/Documents/Interpreter/Token.cpp" "/home/wolf/Documents/Interpreter/cmake-build-debug/CMakeFiles/Interpreter.dir/Token.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Interpreter.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Interpreter.dir/Main.cpp.o"
+  "CMakeFiles/Interpreter.dir/Token.cpp.o"
+  "CMakeFiles/Interpreter.dir/Scanner.cpp.o"
+  "CMakeFiles/Interpreter.dir/Printer.cpp.o"
+  "CMakeFiles/Interpreter.dir/Expressions/Assign.cpp.o"
+  "CMakeFiles/Interpreter.dir/Expressions/Binary.cpp.o"
+  "CMakeFiles/Interpreter.dir/Expressions/Call.cpp.o"
+  "CMakeFiles/Interpreter.dir/Expressions/Get.cpp.o"
+  "CMakeFiles/Interpreter.dir/Expressions/Grouping.cpp.o"
+  "CMakeFiles/Interpreter.dir/Expressions/Literal.cpp.o"
+  "CMakeFiles/Interpreter.dir/Expressions/Logical.cpp.o"
+  "CMakeFiles/Interpreter.dir/Expressions/Set.cpp.o"
+  "CMakeFiles/Interpreter.dir/Expressions/Super.cpp.o"
+  "CMakeFiles/Interpreter.dir/Expressions/This.cpp.o"
+  "CMakeFiles/Interpreter.dir/Expressions/Unary.cpp.o"
+  "CMakeFiles/Interpreter.dir/Expressions/Variable.cpp.o"
+  "Interpreter.pdb"
+  "Interpreter"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Interpreter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
